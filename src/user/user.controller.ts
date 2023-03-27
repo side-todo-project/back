fd6ab6c8e7bdbc91e6a8aa351e18f12b03381f74
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-03-27 18:22:44 +0900

    #40 Fix-[fix req user]

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { ApiBody, ApiHeader, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { Public } from 'src/auth/skip-auth.decorator';
+
 import { UserService } from './user.service';
 import { UserInfoRequestDto } from './request.dto/userInfo.request.dto';
 import { SetNicknameRequestDto } from './request.dto/setNickname.request.dto';
@@ -18,7 +18,6 @@ import { followRequestDto } from './request.dto/follow.request.dto';
 import { searchFollowRequestDto } from './request.dto/searchFollow.request.dto';
 import { followerResponseDto } from './response.dto/follower.response.dto';
 import { followingResponseDto } from './response.dto/following.response.dto';
-import { scheduleResponseDto } from "../schedule/dto/schedule.response.dto";
 
 
 @ApiTags('user')
@@ -36,29 +35,29 @@ export class UserController {
     return 'success';
   }
 
-  @Public()
+  @UseGuards(JwtAuthGuard)
   @Get('/follower')
   @ApiOperation({ summary: '나를 팔로우 하는 유저 목록 가져오기' })
   @ApiResponse({
     type: followerResponseDto,
   })
-  async getFollower(@Req() req, @Body() data) {
-    const followers = await this.usersService.getFollower(data.userId);
+  async getFollower(@Req() req) {
+    const followers = await this.usersService.getFollower(req.user.userId);
     return new followerResponseDto(followers);
   }
 
-  @Public()
+  @UseGuards(JwtAuthGuard)
   @Get('/following')
   @ApiOperation({ summary: '내가 팔로우 하는 유저 목록 가져오기' })
   @ApiResponse({
     type: followingResponseDto,
   })
   async getFollowing(@Req() req) {
-    const followings = await this.usersService.getFollowing(req.userId);
+    const followings = await this.usersService.getFollowing(req.user.userId);
     return new followingResponseDto(followings);
   }
 
-  @Public()
+  @UseGuards(JwtAuthGuard)
   @Post('/follower')
   @ApiBody({ type: searchFollowRequestDto })
   @ApiOperation({ summary: '나를 팔로우 하는 사람 중 특정 닉네임 검색하기' })
@@ -74,7 +73,7 @@ export class UserController {
     return new followerResponseDto(followers);
   }
 
-  @Public()
+  @UseGuards(JwtAuthGuard)
   @Post('/following')
   @ApiBody({ type: searchFollowRequestDto })
   @ApiOperation({ summary: '내가 팔로우 하는 사람 중 특정 닉네임 검색하기' })
@@ -95,7 +94,7 @@ export class UserController {
   @ApiOperation({ summary: '유저 팔로우 하기' })
   @ApiBody({ type: followRequestDto })
   async follow(@Req() req, @Body() data: followRequestDto) {
-    await this.usersService.follow(req.userId, data.userId);
+    await this.usersService.follow(req.user.userId, data.userId);
     return 'success';
   }
 
@@ -104,7 +103,7 @@ export class UserController {
   @ApiOperation({ summary: '유저 언팔로우 하기' })
   @ApiBody({ type: followRequestDto })
   async unfollow(@Req() req, @Body() data: followRequestDto) {
-    await this.usersService.unfollow(req.userId, data.userId);
+    await this.usersService.unfollow(req.user.userId, data.userId);
     return 'success';
   }
 
@@ -120,6 +119,6 @@ export class UserController {
   @ApiOperation({ summary: '유저 정보 보여주기' })
   @ApiBody({ type: UserInfoRequestDto })
   async info(@Req() req) {
-    return this.usersService.getUserInfo(req.id);
+    return this.usersService.getUserInfo(req.user.userId);
   }
 }
